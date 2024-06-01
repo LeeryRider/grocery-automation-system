e88dfbbeace6818adfe6b30c3879bc5fdf83c20d
Kemal Eren Hassu <150674296+LeeryRider@users.noreply.github.com>
2024-06-01 19:05:29 +0300

    Add files via upload

---
+++ DAL/DAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DAL
+{
+    public class DAL
+    {
+        public DAL() { }
+        static DBManavEntities dbe = new DBManavEntities();
+        public static void GetAllInfo(DataGridView dgv)
+        {
+            var query = from x in dbe.TBLUrunler select new { x.UrunID, x.UrunAd, x.UrunFiyat, x.Tur , x.Stok, x.Maliyet };
+            dgv.DataSource = query.ToList();
+        }
+    }
+}
